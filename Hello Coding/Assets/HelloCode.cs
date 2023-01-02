9930a5f48bc7dfc63820ee3d575f1ee56018fb11
jooo0922 <2014102458@khu.ac.kr>
2023-01-03 01:23:35 +0900

    feat: 유니티 기본 변수 타입 출력

--- Hello Coding/Assets/HelloCode.cs
+++ Hello Coding/Assets/HelloCode.cs
@@ -9,5 +9,19 @@ public class HelloCode : MonoBehaviour
     {
         // Hello World!를 콘솔에 출력 
         Debug.Log("Hello World!");
+
+        // 캐릭터의 프로필을 변수로 만들기
+        string characterName = "라라";
+        char bloodType = 'A';
+        int age = 17;
+        float height = 168.3f;
+        bool isFemale = true;
+
+        // 생성한 변수를 콘솔에 출력
+        Debug.Log("캐릭터 이름 : " + characterName);
+        Debug.Log("혈액형 : " + bloodType);
+        Debug.Log("나이 : " + age);
+        Debug.Log("키 : " + height);
+        Debug.Log("여성인가? : " + isFemale);
     }
 }
