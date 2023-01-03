cb5d1c60ea2888573025272b710c8797e8277656
jooo0922 <2014102458@khu.ac.kr>
2023-01-03 21:13:51 +0900

    feat: 조건문 에제 코드 추가

--- Hello Coding/Assets/HelloCode.cs
+++ Hello Coding/Assets/HelloCode.cs
@@ -7,7 +7,16 @@ public class HelloCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int love = 100;
 
+        if (love > 70)
+        {
+            Debug.Log("굿엔딩: 히로인과 사귀게 되었다!");
+        }
+        if (love <= 70)
+        {
+            Debug.Log("배드엔딩: 히로인에게 차였다.");
+        }
     }
 
 }
