e24d851f1834a77526867b750fee4336783f8655
jooo0922 <2014102458@khu.ac.kr>
2023-01-03 21:22:37 +0900

    remove: 기존 예제코드 제거

--- Hello Coding/Assets/HelloCode.cs
+++ Hello Coding/Assets/HelloCode.cs
@@ -7,20 +7,7 @@ public class HelloCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int love = 80;
 
-        if (love > 90)
-        {
-            Debug.Log("트루엔딩: 히로인과 결혼했다!");
-        }
-        else if (love > 70)
-        {
-            Debug.Log("굿엔딩: 히로인과 사귀게 되었다!");
-        }
-        else
-        {
-            Debug.Log("배드엔딩: 히로인에게 차였다.");
-        }
     }
 
 }
