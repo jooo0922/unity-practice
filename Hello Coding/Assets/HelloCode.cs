0ac5ba4b10a8c9098518e802a47738d0025e44e4
jooo0922 <2014102458@khu.ac.kr>
2023-01-03 21:51:43 +0900

    remove: 논리연산자 예제코드 제거

--- Hello Coding/Assets/HelloCode.cs
+++ Hello Coding/Assets/HelloCode.cs
@@ -7,17 +7,7 @@ public class HelloCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int age = 11;
 
-        if (age > 7 && age < 18)
-        {
-            Debug.Log("의무 교육을 받고 있습니다.");
-        }
-
-        if (age < 13 || age > 70)
-        {
-            Debug.Log("일을 할 수 없는 나이입니다.");
-        }
     }
 
 }
