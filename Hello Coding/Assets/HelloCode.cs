a5c69d67b6bd06c2270af6163951feb393eeec93
jooo0922 <2014102458@khu.ac.kr>
2023-01-03 21:56:46 +0900

    remove: for문 예제코드 제거

--- Hello Coding/Assets/HelloCode.cs
+++ Hello Coding/Assets/HelloCode.cs
@@ -7,10 +7,7 @@ public class HelloCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            Debug.Log(i + "번째 순번입니다.");
-        }
+
     }
 
 }
