388627e04559acb3b93a859b806884f99944b117
jooo0922 <2014102458@khu.ac.kr>
2023-01-03 22:04:02 +0900

    feat: while 문으로 체력깎기 예제코드 추가

--- Hello Coding/Assets/HelloCode.cs
+++ Hello Coding/Assets/HelloCode.cs
@@ -7,7 +7,22 @@ public class HelloCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        bool isDead = false;
+        int hp = 100;
 
+        while(!isDead)
+        {
+            Debug.Log("현재 체력 : " + hp);
+
+            hp = hp - 33;
+
+            if (hp <= 0)
+            {
+                isDead = true;
+                Debug.Log("플레이어는 죽었습니다.");
+            }
+
+        }
     }
 
 }
