58af8b8f6472af8718bb91407fdee2f693290060
jooo0922 <2014102458@khu.ac.kr>
2023-01-03 21:58:41 +0900

    feat: while문 예제코드 추가

--- Hello Coding/Assets/HelloCode.cs
+++ Hello Coding/Assets/HelloCode.cs
@@ -7,7 +7,12 @@ public class HelloCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        int i = 0;
+        while (i < 10)
+        {
+            Debug.Log(i + "번째 루프입니다.");
+            i++;
+        }
     }
 
 }
