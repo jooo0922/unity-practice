1f36d5fa35eeefe667f954b96a76d0c4a3915387
jooo0922 <2014102458@khu.ac.kr>
2023-01-11 00:54:03 +0900

    feat: jerry 변수의 참조값 변경 후 멤버변수 덮어쓰기

--- Hello Class/Assets/Zoo.cs
+++ Hello Class/Assets/Zoo.cs
@@ -15,6 +15,9 @@ public class Zoo : MonoBehaviour
         jerry.name = "제리";
         jerry.sound = "찍찍!";
 
+        jerry = tom; // jerry 가 가리키는 참조값을 tom 이 가리키는 참조값으로 변경 
+        jerry.name = "미키"; // jerry 가 가리키는 인스턴스가 바뀐 상태에서, 그 인스턴스의 name 값을 바꾸고 있음!
+
         tom.PlaySound();
         jerry.PlaySound();
     }
