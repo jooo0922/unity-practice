6e9bb9f978226f4125c7c734e8a9a0fa1275ab30
jooo0922 <2014102458@khu.ac.kr>
2023-01-11 00:01:38 +0900

    feat: Animal 클래스 변수 및 메서드 추가

--- Hello Class/Assets/Animal.cs
+++ Hello Class/Assets/Animal.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animal : MonoBehaviour
+// 순수한 클래스의 역할만 살펴보기 위해 Animal 클래스는 MonoBehaviour 클래스를 상속받지 않음 -> 컴포넌트로써의 기능 상실!
+public class Animal
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 동물에 대한 변수
+    public string name;
+    public string sound;
 
-    // Update is called once per frame
-    void Update()
+    // 울음소리를 재생하는 메서드
+    public void PlaySound()
     {
-        
+        Debug.Log(name + " : " + sound);
     }
+        
 }
