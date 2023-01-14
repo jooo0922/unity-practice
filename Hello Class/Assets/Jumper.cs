80774c6bd3bfe4d6c719677e151468ee63aec93b
jooo0922 <2014102458@khu.ac.kr>
2023-01-14 22:08:00 +0900

    feat: Jumper 스크립트 추가

---
+++ Hello Class/Assets/Jumper.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    public Rigidbody myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody.AddForce(0, 500, 0);
+    }
+}
