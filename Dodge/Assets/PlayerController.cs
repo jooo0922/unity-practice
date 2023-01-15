a28c1f2988da537c8141c250bc0474c4778a5113
jooo0922 <2014102458@khu.ac.kr>
2023-01-16 00:15:08 +0900

    feat: Player 게임 오브젝트를 조종하는 스크립트 생성

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    public float speed = 8f; // 이동 속력
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
