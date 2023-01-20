a8bad09effbe8bac091c9473fc4b4c93ae9d721d
jooo0922 <2014102458@khu.ac.kr>
2023-01-21 00:50:07 +0900

    feat: Bullet 게임 오브젝트에 적용할 스크립트 생성

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f; // 탄알 이동 속력
+    private Rigidbody bulletRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
