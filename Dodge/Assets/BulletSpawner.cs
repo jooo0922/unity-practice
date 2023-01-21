fe90e47f87caddecc8e5f528c51e666c2bc47ecc
jooo0922 <2014102458@khu.ac.kr>
2023-01-21 23:54:39 +0900

    feat: Start() 메서드에서 멤버변수 값 설정 및 초기화

--- Dodge/Assets/BulletSpawner.cs
+++ Dodge/Assets/BulletSpawner.cs
@@ -15,12 +15,19 @@ public class BulletSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 마지막 탄알 생성 시점 이후의 누적시간 값을 0으로 초기화
+        timeAfterSpawn = 0f;
+
+        // 탄알 생성 간격을 'spawnRateMin ~ spawnRateMax' 사이의 랜덤값으로 지정 (범위값을 실수로 받으면 리턴값도 실수를 전달함)
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+
+        // PlayerController 컴포넌트를 갖고있는 게임 오브젝트를 찾고, 해당 오브젝트의 Transform 컴포넌트 참조값을 멤버변수에 할당함. -> 탄알이 향할 목표 게임 오브젝트의 위치를 알기 위함.
+        target = FindObjectOfType<PlayerController>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
+    }                                                                                                                                                                          
 }
