60c4bd8e209b9322ed35270500ce03b53b3170ae
jooo0922 <2014102458@khu.ac.kr>
2023-01-21 16:00:57 +0900

    feat: 트리거 충돌 시 발생할 이벤트 메서드에서 대응 동작 정의

--- Dodge/Assets/Bullet.cs
+++ Dodge/Assets/Bullet.cs
@@ -18,4 +18,24 @@ public class Bullet : MonoBehaviour
         // 3초 뒤에 자신의 게임 오브젝트 파괴 (두 번째 인자값이 Destroy() 메서드 실행 지연시간)
         Destroy(gameObject, 3f);
     }
+
+    // 트리거 충돌 시 자동으로 실행되는 이벤트 메서드 -> 실제 물리적인 충돌이 아니여도, 최소 한 개의 게임 오브젝트가 트리거 콜라이더라서 '충돌 감지'만 일어나면 발생!
+    private void OnTriggerEnter(Collider other)
+    {
+        // 충돌한 상대방 게임 오브젝트가 Player 태그를 가진 경우 -> 즉, 상대방이 Player 게임 오브젝트인지 확인하려는 것! (tag 변수는 각 게임 오브젝트의 컴포넌트에도 존재!)
+        if (other.tag == "Player")
+        {
+            // 상대방 게임 오브젝트에서 PlayerController 컴포넌트 가져오기
+            // c# script 컴포넌트를 GetComponent() 로 찾고자 한다면, 제네릭 타입을 해당 스크립트에서 MonoBehavior 클래스로부터 상속받고 있는 클래스 이름으로 넣어주면 되겠지!
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            // 상대방으로부터 PlayerController 컴포넌트를 가져오는 데 성공했다면(즉, 상대방 게임 오브젝트에 PlayerController 라는 컴포넌트가 존재한다면~)
+            if (playerController != null)
+            {
+                // 상대방 PlayerController 컴포넌트의 Die() 메서드 실행
+                playerController.Die();
+            }
+        }
+    }
 }
+
