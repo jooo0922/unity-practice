9422dd3b38d48592e5820b3a096a142872dd7b3c
jooo0922 <2014102458@khu.ac.kr>
2023-01-22 01:54:16 +0900

    feat: Update() 메서드에서 랜덤한 주기에 따라 탄알 게임 오브젝트 복사하여 생성하는 로직 추가

--- Dodge/Assets/BulletSpawner.cs
+++ Dodge/Assets/BulletSpawner.cs
@@ -28,6 +28,26 @@ public class BulletSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // timeAfterSpawn 시간값 누적
+        // Time.deltaTime -> 직전 프레임이 그려진 시점에서 현재 프레임이 그려진 시간까지의 시간 간격
+        timeAfterSpawn += Time.deltaTime;
+
+        // 최근 탄알 생성 시점에서부터 누적된 시간값이 랜덤하게 할당된 탄알 생성 주기값을 넘어서면
+        if (timeAfterSpawn >= spawnRate)
+        {
+            // 누적된 시간값 초기화
+            timeAfterSpawn = 0f;
+
+            // bulletPrefab 원본을 복제한 탄알 게임 오브젝트 생성
+            // 또한 복제된 탄알의 위치 및 회전값을 BulletSpawner(탄알 생성기) 자신의 Transform 컴포넌트와 맞춤 -> why? 탄알이 탄알생성기에서 튀어나오니까!
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            // 복제하여 생성된 탄알 게임 오브젝트의 정면 방향이 target (즉, Player 게임 오브젝트) 를 바라보도록 Transform.LookAt() 을 사용하여 회전시킴
+            // LookAt 의 인자에는 바라보고자 하는 게임 오브젝트의 Transform 컴포넌트를 전닳해야 함. -> Start() 메서드에서 target 변수에 Player 게임 오브젝트의 Transform 컴포넌트를 할당했었지?
+            bullet.transform.LookAt(target);
+
+            // 다음번 탄알 생성 주기를 또 다른 랜덤값으로 변경해 줌.
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
     }                                                                                                                                                                          
 }
