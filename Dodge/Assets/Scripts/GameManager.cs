344e15bd05ae4f953237b87c7151852e06af79d5
jooo0922 <2014102458@khu.ac.kr>
2023-01-24 02:53:04 +0900

    feat: GameManager 모듈 생성

---
+++ Dodge/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // 유니티 UI 시스템 관련 라이브러리 가져오기
+using UnityEngine.SceneManagement; // 씬 관리 관련 라이브러리 -> 게임 도중 씬 재시작 기능 구현 목적
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
