ffa26d97e985612d696fca160c46db436300f7f4
jooo0922 <2014102458@khu.ac.kr>
2023-01-24 03:14:28 +0900

    feat: 생존시간 업데이트 및 표시 로직 추가

--- Dodge/Assets/Scripts/GameManager.cs
+++ Dodge/Assets/Scripts/GameManager.cs
@@ -24,7 +24,16 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 플레이어가 생존해있어 게임오버가 아닌 상태에서만 아래 갱신 로직 실행
+        if (!isGameover)
+        {
+            // 생존 시간을 델타타임(프레임 간 시간간격)을 누적하여 업데이트함.
+            surviveTime += Time.deltaTime;
+
+            // 갱신된 생존시간을 float -> int 로 형변환 후, Time Text 게임 오브젝트 > 텍스트 컴포넌트 > 텍스트 필드에 할당해 줌.
+            // 형변환을 해준 이유는, float 실수 타입은 소수점이 길이서, 이 긴 숫자들이 전부 화면에 표시되지 않도록 정수형으로 끊어서 표시하려는 것임.
+            timeText.text = "Time: " + (int)surviveTime;
+        }
     }
 
     // 현재 게임을 게임오버 상태로 변경하는 메서드 (상태값 변경 메서드) -> 일단 메서드 선언만 해둠
