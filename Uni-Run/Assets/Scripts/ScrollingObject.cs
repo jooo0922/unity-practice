db98e37b08ee05bc9a20b9314cf5959336e50386
jooo0922 <2014102458@khu.ac.kr>
2023-02-09 20:34:12 +0900

    feat: 발판 게임 오브젝트와 배경 게임 오브젝트를 초당 speed 속도로 왼쪽으로 평행이동 시키는 로직 추가

--- Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -6,5 +6,9 @@ public class ScrollingObject : MonoBehaviour {
 
     private void Update() {
         // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        // 초당 Vector3(-speed, 0, 0) 만큼 왼쪽으로 평행이동
+        // 참고로, Vector3.left = Vector3(-1, 0, 0) 방향벡터의 속기 표현이고,
+        // Translate() 메서드는 게임 오브젝트의 오브젝트 공간(또는 설정에 따라 전역공간) 기준으로 인자로 전달한 Vector3 만큼 평행이동시킴!
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
