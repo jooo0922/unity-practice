5d5defa1085d7c509b95cbc66d1d9d57bdc33c09
jooo0922 <2014102458@khu.ac.kr>
2023-02-09 22:28:28 +0900

    feat: 박스콜라이더2D 컴포넌트로부터 배경 게임 오브젝트의 가로너비를 구하는 로직 추가

--- Uni-Run/Assets/Scripts/BackgroundLoop.cs
+++ Uni-Run/Assets/Scripts/BackgroundLoop.cs
@@ -4,8 +4,13 @@
 public class BackgroundLoop : MonoBehaviour {
     private float width; // 배경의 가로 길이
 
+    // Awake() 는 Start() 와 유사하게 초기 1회 자동 실행되는 유니티 이벤트 메서드
+    // 다만, Start() 보다 실행시점이 한 프레임 더 빠름
     private void Awake() {
         // 가로 길이를 측정하는 처리
+        // BoxCollider2D 컴포넌트를 가져와서, 사이즈 값을 참조해서 sky 게임 오브젝트의 width 를 구함.
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x; // 참고로 BoxCollider2D 의 사이즈값은 Sky 게임 오브젝트의 인스펙터 창에서도 확인 가능!
     }
 
     private void Update() {
