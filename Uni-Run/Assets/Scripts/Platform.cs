76b7e7a170405cd4b940a4641401c93ac3d51097
jooo0922 <2014102458@khu.ac.kr>
2023-02-12 16:14:32 +0900

    feat: 컴포넌트 활성화 시 발판 정보를 리셋하는 로직 추가

--- Uni-Run/Assets/Scripts/Platform.cs
+++ Uni-Run/Assets/Scripts/Platform.cs
@@ -6,8 +6,27 @@ public class Platform : MonoBehaviour {
     private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
 
     // 컴포넌트가 활성화될때 마다 매번 실행되는 메서드
+    // 인스펙터 창에서 컴포넌트 옆의 체크버튼을 활성화/비활성화하거나, 해당 컴포넌트가 포함된 게임 오브젝트 옆의 체크버튼을 활성화/비활성화해서
+    // 모든 컴포넌트가 일률적으로 활성화/비활성화될 때 실행되는 유니티 이벤트 메서드
     private void OnEnable() {
         // 발판을 리셋하는 처리
+        // 밟힘 상태를 리셋함
+        stepped = false;
+
+        // 인스펙터 창에서 할당된 장애물 게임 오브젝트 리스트 배열을 반복 순회
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            // Random.Range(0, 3) 은 0에서 3 사이의 정수 0, 1, 2 중 하나를 랜덤으로 반환함
+            if (Random.Range(0, 3) == 0)
+            {
+                // 현재 순회를 돌고 있는 장애물 게임 오브젝트를 1/3 확률로 활성화함
+                obstacles[i].SetActive(true);
+            } else
+            {
+                // 반대로 현재 장애물 게임 오브젝트를 2/3 확률로 비활성화함
+                obstacles[i].SetActive(false);
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
