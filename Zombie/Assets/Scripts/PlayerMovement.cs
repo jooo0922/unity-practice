66877e73906743caef6c6d0a908d329778e78e0f
jooo0922 <2014102458@khu.ac.kr>
2023-02-17 23:07:59 +0900

    feat: 물리 갱신 주기에 맞춰 실행되는 업데이트 루프 FixedUpdate() 로직 추가

--- Zombie/Assets/Scripts/PlayerMovement.cs
+++ Zombie/Assets/Scripts/PlayerMovement.cs
@@ -18,8 +18,20 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
+    // 유니티 내부의 물리 엔진 실행주기가 0.2로 고정되어 있어, 이 주기에 맞춰 실행되는 업데이트 루프임.
+    // 게임오브젝트의 이동, 충돌 등 물리적인 변화 처리 시, 물리 엔진의 예측을 방해하지 않도록 FixedUpdate() 사용이 권장됨.
     private void FixedUpdate() {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+        // 회전 실행
+        Rotate();
+
+        // 이동 실행
+        Move();
+
+        // 입력값에 따라 애니메이터 컴포넌트 > 애니메이터 컨트롤러 > Move 파라미터 값 변경
+        // 이 값을 애니메이터 컨트롤러 > Movement 상태의 블렌드 트리 > 각 애니메이션 클립별 Threshold 값과 비교해서
+        // 기본 애니메이션 상태인 Movement 내의 클립들을 적절한 비율로 섞어서 재생시켜 줌.
+        playerAnimator.SetFloat("Move", playerInput.move); // PlayerInput 컴포넌트의 move 프로퍼티 get 접근자 사용해서 값을 가져옴.
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
