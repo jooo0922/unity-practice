9c2c6c4ca80293ecfa9f0788376b3be8d12790be
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-03-24 22:28:42 +0900

    feat: 단일 에셋을 코루틴 메서드에서 비동기 로드 처리

--- Addressable System/Assets/Scripts/AddressableSystemManager.cs
+++ Addressable System/Assets/Scripts/AddressableSystemManager.cs
@@ -6,15 +6,31 @@ using UnityEngine.ResourceManagement.AsyncOperations; // 비동기 처리 사용
 
 public class AddressableSystemManager : MonoBehaviour
 {
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(PlayMainMusicRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // 코루틴 메서드로 정의 -> why? 에셋을 비동기로 로드하는 동안 대기시간을 주려고
+    private IEnumerator PlayMainMusicRoutine()
+    {
+        AsyncOperationHandle<AudioClip> operationHandle = Addressables.LoadAssetAsync<AudioClip>("main_music");
+
+        yield return operationHandle; // 에셋을 가져오는 오퍼레이션을 기다리기 -> 코루틴 메서드 사용 이유
+        AudioClip musicClip = operationHandle.Result; // 비동기 오퍼레이션 결과값인 오디오 클립 가져오기
+
+        // 오디오 소스를 통해 재생
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = musicClip;
+        audioSource.Play();
     }
 }
