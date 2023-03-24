5ce140e79416878ae3c76987708c8ea3468a60be
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-03-25 01:15:20 +0900

    feat: AssetReference 타입으로부터 에셋 로드하는 로직 추가

--- Addressable System/Assets/Scripts/MusicPlayer.cs
+++ Addressable System/Assets/Scripts/MusicPlayer.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; // 익명함수 콜백을 등록하기 위한 Action 타입 변수 선언을 위해 선언된 네임스페이스 
+using UnityEngine.AddressableAssets; // 어드레서블 시스템 API 사용을 위해 선언된 네임스페이스
+using UnityEngine.ResourceManagement.AsyncOperations; // 비동기 처리 사용을 위해 선언된 네임스페이스
 
 public class MusicPlayer : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AssetReference musicClipReference;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,20 @@ public class MusicPlayer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayMusic()
+    {
+        // AssetReference 타입으로부터 에셋을 가져올 때에는, 별도의 어드레스 문자열을 입력하지 않고 바로 LoadAssetAsync() 를 호출하면 됨.
+        // 왜냐, 이미 AssetReference 타입 변수에 GUID 가 할당되어 있으니까!
+        AsyncOperationHandle<AudioClip> operationHandle =
+            musicClipReference.LoadAssetAsync<AudioClip>();
+
+        operationHandle.Completed += (operation) =>
+        {
+            AudioClip audioClip = operation.Result;
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        };
     }
 }
