b1d2891b24105fef507de29780f1ac0cf3f7a26d
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-03-24 22:16:14 +0900

    feat: 어드레서블 시스템 사용을 위해 선언된 네임스페이스들

--- Addressable System/Assets/Scripts/AddressableSystemManager.cs
+++ Addressable System/Assets/Scripts/AddressableSystemManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets; // 어드레서블 시스템 API 사용을 위해 선언된 네임스페이스
+using UnityEngine.ResourceManagement.AsyncOperations; // 비동기 처리 사용을 위해 선언된 네임스페이스
 
 public class AddressableSystemManager : MonoBehaviour
 {
